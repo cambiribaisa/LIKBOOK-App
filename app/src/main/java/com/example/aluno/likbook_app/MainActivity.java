a90ed8ce136d00bba3c804d2f7e8f15fba08e7f6
aluno <miludlla15@gmail.com>
2019-08-12 11:29:05 -0300

    sdkjhfczsalkdshvlskdfv

--- app/src/main/java/com/example/aluno/likbook_app/MainActivity.java
+++ app/src/main/java/com/example/aluno/likbook_app/MainActivity.java
@@ -1,13 +1,61 @@
 package com.example.aluno.likbook_app;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton camera;
+    private ImageView foto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[](Manifest.permission.CAMERA), 0);
+        }
+
+        foto = (ImageView)  findViewById(R.id.imageViewFoto);
+        findViewById(R.id.buttonCamera).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                tirarFoto();
+
+            }
+
+        });
+
     }
+
+    public void tirarFoto() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 1);
+    }
+
+
+    @Override
+    protected void onActivit  {
+
+    }
+    }
+
+
+//https://www.youtube.com/watch?v=1oyvdqc_QZg
+
+
+
 }
